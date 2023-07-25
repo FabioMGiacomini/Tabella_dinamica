5037caacef65ee0637aab470b0032fb6510f5ff3
Fabio Giacomini <info@viarete.it>
2023-07-25 14:24:00 +0200

    Update index.js
    Aggiunte funzioni per la paginazione. Pulito codice

--- src/index.js
+++ src/index.js
@@ -5,119 +5,122 @@
 *
 * Credits: Thanks to Marko Manojlovic (https://manoylo.ca/) for building the tool (https://random-data-api.com) 
 * Tags: json, fetch, table
-*/
-const tabella = document.getElementById('rebeltable')
-const tbody = tabella.getElementsByTagName('tbody')[0]
-const tdhead = tabella.getElementsByTagName('thead')[0].children[0].children
-const selectRighe = document.getElementById('righeTabella')
-let tabellaDati = []
-let tabellaGriglia = []
- 
+*/ 
+
 let indirizzo = new URL('https://random-data-api.com/api/v2/users')
-indirizzo.searchParams.set('size', 20)
+indirizzo.searchParams.set('size', 40)
 indirizzo.searchParams.set('response_type', 'json')
  
+const numeroRecord = indirizzo.searchParams.get('size')
+
+// recupero i dati da un indirizzo remoto 
 async function getUser(url){
     const response = await fetch(url)
     const resolve = await response.json()
-    createTable(resolve)
+    createTableArray(resolve) 
 }
 getUser(indirizzo)
 
-function createTable(data) { 
-    /**
-     * creo una tabella vuota, e la riempio con gli oggetti presenti
-     * nel file json che arriva dalla sorgente, estraggo quindi fra i tanti dati presenti
-     * nome, cognome, data di nascita, email, qualifica, username, password
-     */
+const tabella = document.getElementById('rebeltable')
+const tbody = tabella.getElementsByTagName('tbody')[0]
+const thead = tabella.getElementsByTagName('thead')[0].childNodes[1]
+const numeroColonne = thead.children.length
+const selectRighe = document.getElementById('righeTabella')
+const singleCell = tbody.getElementsByTagName("td")  
+let tabellaDati = []  
+let valoreDellaSelect = selectRighe.value // inizialmente è 10 grazie all'attributo selected della select
+ 
+function createTableArray(data) {  
     for(let i=0; i<data.length; i++) { 
-       tabellaDati.push([[
+       tabellaDati.push([
         data[i].first_name, 
         data[i].last_name, 
         data[i].date_of_birth, 
         data[i].email, 
         data[i].employment.title,
         data[i].username, 
-        data[i].password]])
-    } 
-    // visualizzo nella tabella come default le prime 10 righe
-    for (let index = 0; index < 10; index++) {  
-        const riga = document.createElement('tr')
-        for(let k = 0; k <tdhead.length; k++) {  
-            const cella = document.createElement('td')  
-            riga.appendChild(cella)
-        }
-        tbody.appendChild(riga)
-    }
-    /**
-     * inserisco i dati nella tabella vuota appena creata, usando i cicli for 
-     * che iterano per multipli di 7 (numero di colonne presenti nella tabella);
-     * se non faccio prima un controllo sull'esistenza della cella con 
-     * la if() mi esce un errore in console anche se lo scipt funziona lo stesso 
-     * 
-     * inizializzo numeroRighe = 10 per popolare di default 
-     * le prime 70 celle (righe per colonne) 
-     */
-    const singleCell = tbody.getElementsByTagName("td")
-       
-    let numeroRighe = 10
-    for (let index = 0; index < numeroRighe; index++) { 
-        if(singleCell[7*index]) { 
-        singleCell[7*index].innerText = tabellaDati[index][0][0] 
-        } 
-    for (let z = 1; z < 7; z++) {  
-        if(singleCell[(7*index)+z]) {  
-            singleCell[(7*index)+z].innerText = tabellaDati[index][0][z] 
-                } 
-        }
-    }
-    /**
-     * se con la select cambio numero, prima cancello le righe e poi le ricreo
-     * in accordo col numero della select, poi svuoto le celle e le ripopolo 
-     * con i dati richiesti
-     */
-    selectRighe.addEventListener('change', function () {
-        let numeroRighe = selectRighe.value
-
-        // rimuovo tutte le righe 
-        while(tbody.hasChildNodes()){
-            tbody.removeChild(tbody.firstChild)
-        } 
-        // ricreo le righe col valore scelto uguale a numeroRighe
-        for (let index = 0; index < numeroRighe; index++) {  
-            const riga = document.createElement('tr')
-            for(let k = 0; k <tdhead.length; k++) {  
-                const cella = document.createElement('td')  
-                riga.appendChild(cella)
-            }
-            tbody.appendChild(riga)
-        }
-
-        /**
-         * svuoto le celle ad ogni cambiamento della select
-         * altrimenti rimangono tutti i dati 
-         */
-        for (const element of singleCell) {
-            element.innerText = " "
-        }
-        // riempio le celle con le nuove richieste
-        for (let index = 0; index < numeroRighe; index++) { 
-            if(singleCell[7*index]) { 
-            singleCell[7*index].innerText = tabellaDati[index][0][0] 
-            } 
-        for (let z = 1; z < 7; z++) {  
-            if(singleCell[(7*index)+z]) {  
-                singleCell[(7*index)+z].innerText = tabellaDati[index][0][z] 
-                    } 
-            }
-        }
-      
-    })
-
-    
-
+        data[i].password])
+    }  
+    creaTabella(valoreDellaSelect) 
+    creaPaginazione(numeroRecord / valoreDellaSelect, 10) 
 }
 
- 
+function creaTabella(numeroRigheTabella){
+    tbody.innerHTML = ' '
 
+    for(let i=0; i<numeroRigheTabella; i++){
+        var tr = document.createElement('tr')
+
+        for (let k=0; k<numeroColonne; k++) {
+            var td = document.createElement('td')
+            td.innerText = tabellaDati[i][k]
+            tr.appendChild(td)
+        }
+        tbody.appendChild(tr)
+    }   
+} 
  
+const paginazione = document.getElementById('paginazione')
+
+function creaPaginazione(pagg,numeroRighe){  
+    paginazione.innerHTML = ''  
+
+    for (let i = 0; i < pagg; i++){
+        let pagina = document.createElement('a') 
+        pagina.innerText = i + 1
+        pagina.setAttribute('data', numeroRighe) // il valore di data è uguale a quello della select 
+        pagina.style.cursor = 'pointer' 
+        pagina.addEventListener("click", ascolta)    
+        paginazione.appendChild(pagina)
+    }  
+} 
+ 
+selectRighe.addEventListener('change', function () {
+    let numeroRighe = selectRighe.value  
+    let risultatoParziale = numeroRecord / numeroRighe 
+    let risultato = Math.round(risultatoParziale)  
+
+    creaPaginazione(risultato, numeroRighe)  
+    creaTabella(numeroRighe) 
+})   
+
+/**
+ * cliccando sulle àncore della paginazione la funzione legge
+ * il numero di pagina e l'attributo data e li passa alla funzione
+ * che cambia la pagina  
+ */
+function ascolta(e) { 
+    const numeroPagina = e.target.innerText  
+    const suddivisionePagine = e.target.getAttribute("data")  
+    cambiaPagina(numeroPagina, suddivisionePagine)  
+}
+ 
+function cambiaPagina (numeroPagina, suddivisione) {
+    let i = (numeroPagina - 1) * 10
+    let righe = (10+i)
+    if (i === 0 && suddivisione === '20') { 
+        i = 0
+        righe = 20
+    } else if (i === 10 && suddivisione === '20') { 
+        i = 20
+        righe = 40
+    }
+
+    cambiaPaginaSelect(i, righe)
+}
+
+function cambiaPaginaSelect(i, righe){
+    tbody.innerHTML = '' 
+    for(i; i<righe; i++){
+        var tr = document.createElement('tr')
+        for (let k=0; k<numeroColonne; k++) { 
+            var td = document.createElement('td')
+            if (tabellaDati[i][k]) {     
+                 td.innerText = tabellaDati[i][k]
+                 tr.appendChild(td) 
+            }
+        }
+        tbody.appendChild(tr)
+    }   
+    return
+}
